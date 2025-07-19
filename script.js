82c274732450aa9f6b7920d8a248d1871b22e99c
aluno <aluno@embarquedigital.portodigital.local>
2025-07-19 10:16:23 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarferramentas(){
+    //utilizando uma array para listar minhas ferramentas
+const ferramentas = { 'HTML', 'CSS', 'GIT', 'JAVASCRIPT'};
+//criando um metodo de busca das ferramentas 
+let listaFerramentas = document.getElementById(tecnoligia);
+
+}
